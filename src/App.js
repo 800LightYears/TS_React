4d58aad0618a96a9da4b4ad199a18b6120177064
LightYear <huwenbiao2012@163.com>
2020-08-18 23:05:41 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-18 19:57:45
+ * @LastEditTime: 2020-08-18 23:03:00
+ * @LastEditors: LightYear
+ * @Description: In User Settings Edit
+ * @FilePath: \ui\src\App.js
+ */
+
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      1
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: LightYear
+ * @Date: 2020-08-18 19:57:45
+ * @LastEditors: LightYear
+ * @LastEditTime: 2020-08-18 20:30:52
+ * @Description: entry
+ * @FilePath: \ui\src\index.js
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
